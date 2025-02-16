e3b0b9548aa914f0e0fe4258b5ac8365a30d4198
thule2510ddt <lmthu.2510@gmail.com>
2025-02-16 23:23:41 +0700

    add stm32_drivers folder -> support GPIO. SPI, I2C, UART

---
+++ stm32_drivers/Debug/Src/main.d
@@ -0,0 +1,7 @@
+Src/main.o: ../Src/main.c \
+ H:/STM32_Drivers/stm32_drivers/drivers/Inc/stm32f411xx.h \
+ H:/STM32_Drivers/stm32_drivers/drivers/Inc/stm32f411xx_gpio_driver.h \
+ H:/STM32_Drivers/stm32_drivers/drivers/Inc/stm32f411xx.h
+H:/STM32_Drivers/stm32_drivers/drivers/Inc/stm32f411xx.h:
+H:/STM32_Drivers/stm32_drivers/drivers/Inc/stm32f411xx_gpio_driver.h:
+H:/STM32_Drivers/stm32_drivers/drivers/Inc/stm32f411xx.h:
---
+++ stm32_drivers/Debug/drivers/Src/stm32f411xx_gpio_driver.d
@@ -0,0 +1,8 @@
+drivers/Src/stm32f411xx_gpio_driver.o: \
+ ../drivers/Src/stm32f411xx_gpio_driver.c \
+ H:/STM32_Drivers/stm32_drivers/drivers/Inc/stm32f411xx_gpio_driver.h \
+ H:/STM32_Drivers/stm32_drivers/drivers/Inc/stm32f411xx.h \
+ H:/STM32_Drivers/stm32_drivers/drivers/Inc/stm32f411xx_gpio_driver.h
+H:/STM32_Drivers/stm32_drivers/drivers/Inc/stm32f411xx_gpio_driver.h:
+H:/STM32_Drivers/stm32_drivers/drivers/Inc/stm32f411xx.h:
+H:/STM32_Drivers/stm32_drivers/drivers/Inc/stm32f411xx_gpio_driver.h:
